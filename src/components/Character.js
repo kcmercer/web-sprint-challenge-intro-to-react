98a1e45395fcbbf9768b58a94ef347f4d570d1ae
Kenneth Mercer <kmercer5439@gmail.com>
2021-12-10 13:41:02 -0600

    answered questions, added extra styledcomp for fun

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,13 +6,25 @@ font-size: 3rem;
 color: #2A9D8F;
 text-shadow: 2px 2px black;
 text-align: left;
+margin-top: 1%;
+margin-bottom: 2%;
 margin-left: 5%;
+`
+const CharacterP = styled.p`
+font-size: 1rem;
+color: 'black';
+text-align: left;
+margin-top: 1%;
+margin-left: 10%;
 `
 
 function NewCharacters(props) {
   return (
     <div>
       <CharacterH2>{props.name}</CharacterH2>
+      <CharacterP>Birth Year - {props.birth_year}</CharacterP>
+      <CharacterP>Gender - {props.gender}</CharacterP>
+      <CharacterP> Height - {props.height}</CharacterP>
     </div>
   );
 }
